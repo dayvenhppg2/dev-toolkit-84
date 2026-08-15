426cfb4a6d0755a155f6fc8a2d4434139624e0a6
dayvenhppg2 <dayvenhppg208@hotmail.com>
2026-08-15 16:14:28 +0400

    docs:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,37 +1,19 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name, level=logging.INFO):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(handler)
-
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-    def log_performance(self, message, exec_time):
-        self.logger.info(f'{message} - Execution time: {exec_time:.5f} seconds')
-
-custom_logger = CustomLogger('CryptoLogger')
+def setup_logger(log_file='app.log', max_bytes=10*1024*1024, backup_count=5):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    
+    if not logger.hasHandlers():
+        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
 
 # Example usage
 if __name__ == '__main__':
-    import time
-    start_time = time.time()
-    custom_logger.info('Starting performance test')
-    time.sleep(1)  # Simulate some process
-    custom_logger.log_performance('Finished performance test', time.time() - start_time)
+    log = setup_logger()
+    log.info('Logger setup complete.')
