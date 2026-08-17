1a9c6e05c16640fbf5570207904884cade04427f
dayvenhppg2 <dayvenhppg208@hotmail.com>
2026-08-17 16:17:42 +0400

    test:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,24 +1,27 @@
 import logging
-from logging.handlers import RotatingFileHandler
+import logging.handlers
+import os
 
+LOG_DIRECTORY = 'logs'
+LOG_FILE = 'app.log'
+MAX_BYTES = 10 * 1024 * 1024  # 10 MB
+BACKUP_COUNT = 5
 
-def setup_logger(log_file='app.log', max_bytes=10*1024*1024, backup_count=3):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+if not os.path.exists(LOG_DIRECTORY):
+    os.makedirs(LOG_DIRECTORY)
 
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+logger = logging.getLogger('crypto_toolkit')
+logger.setLevel(logging.DEBUG)
 
-    if not logger.handlers:
-        logger.addHandler(handler)
-    return logger
+file_handler = logging.handlers.RotatingFileHandler(
+    os.path.join(LOG_DIRECTORY, LOG_FILE),
+    maxBytes=MAX_BYTES,
+    backupCount=BACKUP_COUNT
+)  
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
+logger.addHandler(file_handler)
 
-# Example usage: if __name__ == '__main__':
-#     log = setup_logger()
-#     log.debug('This is a debug message')
-#     log.info('Info message here')
-#     log.warning('Warning message')
-#     log.error('Error message')
-#     log.critical('Critical issue')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(console_handler)
